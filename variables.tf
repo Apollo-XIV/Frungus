485feea53f2763b0b862afb144cb8a3200ce1d1f
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2023-06-07 09:39:39 +0100

    terraformin and some k8s

--- variables.tf
+++ variables.tf
@@ -6,3 +6,19 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+ variable "subnet_id_1" {
+  type = string
+  default = "subnet-0e102e794b95ed793"
+ }
+ 
+ variable "subnet_id_2" {
+  type = string
+  default = "subnet-07041ac0579e22eee"
+ }
+
+ variable "my_role_arn" {
+   description = "ARN of the IAM role to use"
+   type = string
+   default = "arn:aws:iam::360122086346:role/AmazonEKSClusterPolicy"
+ }
